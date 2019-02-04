e0ee01b9abffc1498530d8966b640cc5875042e7
Muhammad AbuBakar <47162986+abubakar-me@users.noreply.github.com>
2019-02-04 14:38:10 +0500

    Update comments.php

--- comments.php
+++ comments.php
@@ -23,7 +23,7 @@ if ( post_password_required() ) {
 }
 ?>
 
-<div id="comments" class="comments-area">
+<div id="comments" class="comments-area this is new thing">
 
 	<?php
 	// You can start editing here -- including this comment!
